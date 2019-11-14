9b4409da399ee50c6ba9119fe6059b33b746d947
Andriy <andriymattt@gmail.com>
2019-11-14 23:53:19 +0200

    add print methods to Square class

--- src/main/java/com/epam/model/Square.java
+++ src/main/java/com/epam/model/Square.java
@@ -6,6 +6,43 @@ public class Square extends Figure {
         super(size);
     }
 
+    public void print() {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (i != 1 && i != size && j != 1 && j != size) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
 
+    public void printWithDiagonal1() {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (i != 1 && i != size && j != 1 && j != size && j != i) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void printWithDiagonal2() {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (i != 1 && i != size && j != 1 && j != size && j != size + 1 - i) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
 
 }
