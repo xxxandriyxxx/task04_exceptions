1e9adc5ac406c19c96c29632f935c94bda7db927
Andriy <andriymattt@gmail.com>
2019-11-15 01:02:39 +0200

    add Model interface

---
+++ src/main/java/com/epam/model/Model.java
@@ -0,0 +1,21 @@
+package com.epam.model;
+
+public interface Model {
+
+    void printSquare();
+
+    void printSqureWithDiagonal();
+
+    void printSqureWithDiagona2();
+
+    void printTriangle1();
+
+    void printTriangle2();
+
+    void printTriangle3();
+
+    void printTriangle4();
+
+    void printChessBoard();
+    
+}
