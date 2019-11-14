7d911fc0ea6b8c46fb33adf4919bc87c8921990c
Andriy <andriymattt@gmail.com>
2019-11-15 00:16:24 +0200

    add an instance of triangle class to try how its methods work

--- src/main/java/com/epam/Application.java
+++ src/main/java/com/epam/Application.java
@@ -1,16 +1,22 @@
 package com.epam;
 
 import com.epam.model.Square;
+import com.epam.model.Triangle;
 
 public class Application {
 
     public static void main(String[] args) {
 
-        Square sss = new Square(9);
+        Square sss = new Square(10);
         sss.print();
         sss.printWithDiagonal1();
         sss.printWithDiagonal2();
 
+        Triangle ttt = new Triangle(10);
+        ttt.print1();
+        ttt.print2();
+        ttt.print3();
+        ttt.print4();
 
 
     }
