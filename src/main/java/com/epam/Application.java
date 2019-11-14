c6dfca574834888e5de94f9118f81eb7710289e2
Andriy <andriymattt@gmail.com>
2019-11-14 23:56:13 +0200

    add an instance of square class to try how its methods work

--- src/main/java/com/epam/Application.java
+++ src/main/java/com/epam/Application.java
@@ -6,7 +6,10 @@ public class Application {
 
     public static void main(String[] args) {
 
-        Square sss = new Square();
+        Square sss = new Square(9);
+        sss.print();
+        sss.printWithDiagonal1();
+        sss.printWithDiagonal2();
 
 
 
