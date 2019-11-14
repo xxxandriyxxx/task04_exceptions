3520b93ba5c6e8ede60385dfae0230c630191cb3
Andriy <andriymattt@gmail.com>
2019-11-15 00:16:05 +0200

    add print methods to Triangle class

--- src/main/java/com/epam/model/Triangle.java
+++ src/main/java/com/epam/model/Triangle.java
@@ -6,4 +6,52 @@ public class Triangle extends Figure {
         super(size);
     }
 
+    public void print1() {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (i >= j) {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void print2() {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (i <= j) {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void print3() {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (j < i) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void print4() {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (j <= size - i) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
 }
