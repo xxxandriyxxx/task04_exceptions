e6a949638504d00aa656acd99752f099dca02af7
Andriy <andriymattt@gmail.com>
2019-11-15 00:32:33 +0200

    add print method to ChessBoard class

--- src/main/java/com/epam/model/ChessBoard.java
+++ src/main/java/com/epam/model/ChessBoard.java
@@ -5,4 +5,24 @@ public class ChessBoard extends Figure {
     public ChessBoard(int size) {
         super(size);
     }
+
+    public void print() {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (i % 2 == 0) {
+                    if (j % 2 == 0)
+                        System.out.print("* ");
+                    else
+                        System.out.print("- ");
+                } else {
+                    if (j % 2 != 0)
+                        System.out.print("* ");
+                    else
+                        System.out.print("- ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
 }
