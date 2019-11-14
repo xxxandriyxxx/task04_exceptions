7e86eed38f709f3f861d6cfa041fe630e3a1e4fb
Andriy <andriymattt@gmail.com>
2019-11-15 01:14:31 +0200

    implemented methods of Model interface

--- src/main/java/com/epam/model/BusinessLogic.java
+++ src/main/java/com/epam/model/BusinessLogic.java
@@ -1,6 +1,47 @@
 package com.epam.model;
 
-public class BusinessLogic {
+public class BusinessLogic implements Model {
 
+    private Square square;
+    private Triangle triangle;
+    private ChessBoard chessBoard;
+
+    public BusinessLogic(int size) {
+        square = new Square(size);
+        triangle = new Triangle(size);
+        chessBoard = new ChessBoard(size);
+    }
+
+    public void printSquare() {
+        square.print();
+    }
+
+    public void printSquareWithDiagonal1() {
+        square.printWithDiagonal1();
+    }
+
+    public void printSquareWithDiagonal2() {
+        square.printWithDiagonal2();
+    }
+
+    public void printTriangle1() {
+        triangle.print1();
+    }
+
+    public void printTriangle2() {
+        triangle.print2();
+    }
+
+    public void printTriangle3() {
+        triangle.print3();
+    }
+
+    public void printTriangle4() {
+        triangle.print4();
+    }
+
+    public void printChessBoard() {
+        chessBoard.print();
+    }
 
 }
