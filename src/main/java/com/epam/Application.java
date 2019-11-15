06264cb66d1abedffa5ba9109974c742fe23d1fd
Andriy <andriymattt@gmail.com>
2019-11-15 02:41:36 +0200

    use view in Application class

--- src/main/java/com/epam/Application.java
+++ src/main/java/com/epam/Application.java
@@ -1,27 +1,12 @@
 package com.epam;
 
-import com.epam.model.ChessBoard;
-import com.epam.model.Square;
-import com.epam.model.Triangle;
+import com.epam.view.View;
 
 public class Application {
 
     public static void main(String[] args) {
 
-        Square sss = new Square(10);
-        sss.print();
-        sss.printWithDiagonal1();
-        sss.printWithDiagonal2();
-
-        Triangle ttt = new Triangle(10);
-        ttt.print1();
-        ttt.print2();
-        ttt.print3();
-        ttt.print4();
-
-        ChessBoard ccc = new ChessBoard(20);
-        ccc.print();
-
+        new View().show();
 
     }
 }
