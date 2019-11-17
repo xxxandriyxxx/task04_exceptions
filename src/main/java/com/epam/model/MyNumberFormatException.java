53fc8ed46f2f07670574131b09c02e8fc3817226
Andriy <andriymattt@gmail.com>
2019-11-17 18:35:49 +0200

    add custom exception class, that extends NumberFormatException

---
+++ src/main/java/com/epam/model/MyNumberFormatException.java
@@ -0,0 +1,9 @@
+package com.epam.model;
+
+public class MyNumberFormatException extends NumberFormatException {
+
+    public MyNumberFormatException(String message) {
+        super(message);
+    }
+
+}
